3e32863866fbc9b9f0299521d1e5e8e9a258a7ba
klude-au <124860775+klude-au@users.noreply.github.com>
2025-02-04 04:44:38 +1030

    update

--- bravo/-#.php
+++ bravo/-#.php
@@ -1,5 +1,5 @@
 <?php namespace _\pkg\github__klude_org__example_charlie;
 
 class bravo {
-    public function prt(){ echo static::class.": branch/250203-01-dev\n"; }
+    public function prt(){ echo static::class.": feature/250203-01-dev\n"; }
 }
--- v01/plugin-#.php
+++ v01/plugin-#.php
@@ -1,7 +1,7 @@
 <?php namespace _\pkg\github__klude_org__example_charlie\v01;
 
 class plugin {
-    public function prt(){ echo static::class.": branch/250203-01-dev\n"; }
+    public function prt(){ echo static::class.": feature/250203-01-dev\n"; }
 }
 
 
