5e7cc400b08a1ab04abd7349564db8d911d229f0
klude-au <124860775+klude-au@users.noreply.github.com>
2025-02-04 21:46:33 +1030

    update

--- src/alpha/-#.php
+++ src/alpha/-#.php
@@ -1,4 +1,4 @@
-<?php namespace _\pkg\github__klude_org__example_charlie;
+<?php namespace _\pkg\github__klude_org__example_charlie\src;
 
 class alpha {
     const VERSION = "#__FW_PKG__VERSION__#";
